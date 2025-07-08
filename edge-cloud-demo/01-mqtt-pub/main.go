7b4dc5f408a27e65eb71696bd56ac1fb1957194b
PCJ600 <nju_pc@163.com>
2025-07-08 16:57:41 +0800

    fix: solve some bugs

--- edge-cloud-demo/01-mqtt-pub/main.go
+++ edge-cloud-demo/01-mqtt-pub/main.go
@@ -17,8 +17,8 @@ import (
 const (
 	EMQX_SERVER      = "tcp://emqx:1883"
 	BASE_TOPIC       = "device/%s/telemetry"
-	PUBLISH_INTERVAL = 1 * time.Second
-	SIMULATED_DEVICES = 5000
+	PUBLISH_INTERVAL = 500 * time.Millisecond
+	SIMULATED_DEVICES = 10000
 	MAX_RETRIES      = 3
 	KEEPALIVE        = 120
 )
